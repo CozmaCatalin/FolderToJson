8ba17463158468cf178337b4ccec21c5c1668d00
Cozma <cozmacatalin33@gmail.com>
2020-12-03 22:28:03 +0200

    generate extensions statistics

--- main.py
+++ main.py
@@ -9,24 +9,37 @@ def enter_directory():
         else:
             print("Path is not a directory!")
 
-def get_file_extension_statistics(extensions , files):
+def get_file_extension_statistics(extensions , files , size):
     for file in files:
         filename, file_extension = os.path.splitext(file["path"])
         if file_extension not in extensions:
             extensions[file_extension] = {}
             extensions[file_extension]["size"] = file["size"]
-            extensions[file_extension]["numbers"] = 1
+            extensions[file_extension]["count"] = 1
         else:
             extensions[file_extension]["size"] += file["size"]
-            extensions[file_extension]["numbers"] += 1
-    return extensions
+            extensions[file_extension]["count"] += 1
+
+    return get_statistics(extensions,size,len(files))
+
+def get_statistics(extensions,total_size,total_files):
+    statistics = []
+    for attr, value in extensions.items():
+        statistics.append({
+            "size_percent":f'{round((value["size"]*100)/total_size,2)}%',
+            "count_percent":f'{round((value["count"]*100)/total_files,2)}%',
+            "extension":attr,
+            "size":value["size"],
+            "count":value["count"]
+        })
+    return  statistics
 
 def get_directory_content_json_form(directory,json_format):
     json_format["files"] = []
     json_format["size"] = 0
     json_format["files_number"] = 0
     json_format["folders_number"] = 0
-    json_format["files_extensions"] = {}
+    json_format["files_extensions_statistics"] = {}
 
     files = os.listdir(directory)
     for file in files:
@@ -43,11 +56,10 @@ def get_directory_content_json_form(directory,json_format):
             json_format["files"].append({"path":path,"size":size})
             json_format["size"] += size
 
-    json_format["files_extensions"] = get_file_extension_statistics({},json_format["files"])
+    json_format["files_extensions_statistics"] = get_file_extension_statistics({},json_format["files"],json_format["size"])
     json_format["files_number"] = len(json_format["files"])
 
     return json_format
-
 
 my_json = get_directory_content_json_form('E:\Facultate 3\FolderToJson',{})
 print(str(my_json["size"]) + " bytes " + str(my_json["files_number"]) + " files " + str(my_json["folders_number"]) + " folders")
