bcfca1b27a401e7f13a96986f80b55778203f60c
Cozma <cozmacatalin33@gmail.com>
2020-12-03 16:56:36 +0200

    format directory to json and add prettier output

--- main.py
+++ main.py
@@ -1,11 +1,30 @@
 import os
+import json
 
-def enterDirectory():
+def enter_directory():
     while True:
-        pathToDirectory = input("Enter directory: ")
-        if os.path.isdir(pathToDirectory):
-            break
+        path_to_directory = input("Enter directory: ")
+        if os.path.isdir(path_to_directory):
+            return path_to_directory
         else:
             print("Path is not a directory!")
 
-enterDirectory()
+def get_directory_content_json_form(directory,json_format):
+    json_format["files"] = []
+    files = os.listdir(directory)
+    for file in files:
+        if file[0] == "." :
+            continue
+        path = f'{directory}\{file}'
+        if os.path.isdir(path):
+            json_format[file] = get_directory_content_json_form(path,{})
+        else:
+            json_format["files"].append(file)
+
+    return json_format
+
+
+my_json = get_directory_content_json_form('E:\Facultate 3\FolderToJson',{})
+json_string = json.dumps(my_json)
+parsed = json.loads(json_string)
+print(json.dumps(parsed, indent=4, sort_keys=True))
